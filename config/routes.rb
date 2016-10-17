375855c0163cb365bd6df5a9e6e7e8c9498342f5
David Skoda <dskoda1@binghamton.edu>
2016-10-17 00:12:10 -0400

    added index route, simplecov config

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :players
   resources :positions
   resources :teams
+
+  root 'teams#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
